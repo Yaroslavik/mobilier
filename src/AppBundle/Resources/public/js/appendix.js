e9d3861389aef8a7e4226d921fb179e907aa946a
alexsholk <alex.sholk@itmhouse.com>
2015-10-21 15:27:28 +0300

    fix calc

--- src/AppBundle/Resources/public/js/appendix.js
+++ src/AppBundle/Resources/public/js/appendix.js
@@ -27,17 +27,42 @@ $(function () {
     });
 
     // Калькулятор
+    $('#calc input[name="shape"]').on('change', function () {
+        $('.size-inputs input[type="text"]').val('');
+        switch ($('#calc input[name="shape"]:checked').val()) {
+            case 'shape1':
+            case 'shape2':
+                $('#input-size-c').hide();
+                $('#input-size-a, #input-size-b').show();
+                break;
+            case 'shape3':
+                $('#input-size-c, #input-size-b').hide();
+                $('#input-size-a').show();
+                break;
+            case 'shape4':
+                $('#input-size-a, #input-size-b, #input-size-c').show();
+                break;
+        }
+    }).trigger('change');
+
     $('#btn-calculate').on('click', function () {
         var usdbyr = parseInt($('#usd-byr').val());
         var matcost = parseInt($('input[name="matcost"]:checked').val());
-        var size = parseFloat($('#input-size').val());
+        var sizeA = toFloat($('#input-size-a input').val());
+        var sizeB = toFloat($('#input-size-b input').val());
+        var sizeC = toFloat($('#input-size-c input').val());
+        var size = sizeA + sizeB + sizeC;
         var cost = Math.ceil(usdbyr * matcost * size / 1000) * 1000;
         $('#summary').text(
-            isNaN(cost) ?
+            size == 0 || isNaN(cost) ?
                 'Проверьте правильность ввода данных':
                 'Примерная стоимость кухни: ' + cost.toLocaleString() + 'р'
             ).show(300);
     });
+
+    function toFloat(value) {
+        return parseFloat(value.trim().replace(/,/, '.')) || 0;
+    }
 });
 
 $(window).on('load', function () {
